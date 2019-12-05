57d32fa985c45ff94137bccc1c5c590a439c7626
Mark Salisbury <fmark.salisbury@gmail.com>
2019-12-04 21:37:22 -0700

    Modify sample formatter to work for Quick Basic
    The textmate syntax file (syntaxes/asp-vb-net.tmlanguage.json) was copied
    from the textmate VB file that ships with VS code.  It will need some
    modifications for quickbasic but it does a pretty decent job as is.

--- src/extension.ts
+++ src/extension.ts
@@ -4,23 +4,8 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-    // 👎 formatter implemented as separate command
-    vscode.commands.registerCommand('extension.format-foo', () => {
-        const {activeTextEditor} = vscode.window;
-
-        if (activeTextEditor && activeTextEditor.document.languageId === 'foo-lang') {
-            const {document} = activeTextEditor;
-            const firstLine = document.lineAt(0);
-            if (firstLine.text !== '42') {
-                const edit = new vscode.WorkspaceEdit();
-                edit.insert(document.uri, firstLine.range.start, '42\n');
-                return vscode.workspace.applyEdit(edit)
-            }
-        }
-    });
-
     // 👍 formatter implemented using API
-    vscode.languages.registerDocumentFormattingEditProvider('foo-lang', {
+    vscode.languages.registerDocumentFormattingEditProvider('quickbasic-lang', {
         provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
             const firstLine = document.lineAt(0);
             if (firstLine.text !== '42') {
